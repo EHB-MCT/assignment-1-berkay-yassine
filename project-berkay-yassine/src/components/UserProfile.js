49c5aae63660c9f191010cc2e49e47cebc22e9bb
Berkay Unutkan <berkay.unutkan@student.ehb.be>
2024-10-06 11:02:24 +0200

    Set up initial components for the project

---
+++ project-berkay-yassine/src/components/UserProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserProfile({ username }) {
+    return (
+        <div>
+            <h2>{username}'s Profile</h2>
+        </div>
+    );
+}
+
+export default UserProfile;
